d163ad89dca5a6622ec89dde7071a365121f4a27
MorelOskar <103489695+OskarMorel@users.noreply.github.com>
2023-10-17 15:27:59 +0200

    Modification du fichier DefaultItem

--- src/main/java/but/info3/fsil/DefaultItem.java
+++ src/main/java/but/info3/fsil/DefaultItem.java
@@ -3,5 +3,8 @@ package but.info3.fsil;
 /**
  * Class describing a dummy Item.
  */
+
+/* Modification du fichier pour générer un conflit */
 public class DefaultItem implements Item {
+    /* encore une autre modification */
 }
